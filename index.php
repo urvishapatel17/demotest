8006e7a2b889620a8aabf6ccbe95cbafcbc2b935
urvishapatel17 <urvisha.hikebranding@gmail.com>
2019-03-12 12:37:17 +0530

    add changes to files

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+// echo $actual_link;
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,8 +8,9 @@
 </head>
 <body>
 	<nav>
-		<a href="#">home</a>
-		<a href="/test.php">testpage</a>
+		<a href="<?php echo $actual_link; ?>">home</a>
+		<a href="<?php echo $actual_link; ?>/test.php" target="_blank">testpage</a>
+
 	</nav>
 </body>
 </html>
--- test.php
+++ test.php
@@ -6,6 +6,6 @@
 <body>
 	<h2>heelllo</h2>
 	<p>done dona done</p>
-	<a href="/index.php">home</a>
+	<a href="<?php echo $actual_link; ?>">home</a>
 </body>
 </html>
